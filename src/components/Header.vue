19fb332ffd88d777e591e813a9cbf603352a24b1
skmtpalm <hachihachi.8og@gmail.com>
2017-11-02 17:22:25 +0900

    Add Header && import bulma

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="hero is-primary">
+    <div class="container">
+      <div class="hero-body">
+        <h1 class="title">Super Quiz</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
